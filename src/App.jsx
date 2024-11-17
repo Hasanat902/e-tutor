8be30ba6d347fa1b4d23b44eb28f8f9f9c3a54be
Hasanat902 <abulhasanat902@gmail.com>
2024-11-17 10:54:46 +0600

    implement responsive navbar for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar/Navbar"
+import NavbarBanner from "./components/Navbar/NavbarBanner"
 
 function App() {
 
   return (
     <main className="overflow-x-hidden">
       <Navbar />
+      <NavbarBanner />
     </main>
   )
 }
---
+++ src/components/Navbar/NavbarBanner.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+
+const NavbarBanner = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    isOpen && (
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.7 }}
+        className="bg-primary text-sm text-center font-semibold p-1 hidden lg:block relative"
+      >
+        Are you a university or school student for an online tutoring
+        partnership?
+        <a href="#" className="text-secondary ml-2">
+          Talk to us
+        </a>
+        <div
+          onClick={() => setIsOpen(false)}
+          className="absolute top-1/2 right-10 cursor-pointer -translate-y-1/2"
+        >
+          X
+        </div>
+      </motion.div>
+    )
+  );
+};
+
+export default NavbarBanner;
